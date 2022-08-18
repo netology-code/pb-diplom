bc9dabb9f2893187d7528bd8418e80252b5901ee
IrinaNN <irig_nazarova@mail.ru>
2022-08-18 22:56:46 +0300

    solution4

--- logic.js
+++ logic.js
@@ -16,25 +16,6 @@ function startGame(){
 	renderBoard(gameField);
 };
 
-function isWinningSituation(){
-	const N = gameField.length;
-	
-	for (let i=0; i<N; ++i){
-		if (isWinningSeguence(i, i+1,0,0,N,1) ||
-		isWinningSeguence(0,N,1,i,i+1,0)
-		){
-			return true;
-		}
-	}
-	if (isWinningSeguence(0,N,1,0,N,1) ||
-		isWinningSeguence(N-1, -1, -1, 0, N, 1)
-		){
-			return true;
-		}
-	
-	return true;
-}
-
 function isWinningSeguence(r0, r1, ri, c0, c1, ci) {
 	let  firstSymbol = null;
 	
@@ -56,7 +37,28 @@ function isWinningSeguence(r0, r1, ri, c0, c1, ci) {
 	}
 	return true;
 	
+}
+
+function isWinningSituation(){
+	const N = gameField.length;
 	
+	for (let i=0; i<N; ++i){
+		if (
+			isWinningSeguence(i, i+1,0,0,N,1) ||
+			isWinningSeguence(0,N,1,i, i+1,0)
+		){
+		return true;
+		}
+	}
+
+		if (
+			isWinningSeguence(0,N,1,0,N,1) ||
+			isWinningSeguence(N-1, -1, -1, 0, N, 1)
+		){
+		return true;
+}
+	
+	return false;
 }
 
 function click(row, column) {
