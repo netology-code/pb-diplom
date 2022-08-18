3a0b27f0fa1f9e1f58b0b643f2a4b6ea712bae6d
IrinaNN <irig_nazarova@mail.ru>
2022-08-18 21:41:35 +0300

    solution

--- logic.js
+++ logic.js
@@ -9,15 +9,35 @@ function creatGameField(){
 		['','','']
 	]
 };
+
 function startGame(){
-	gameField = creatGameField();
+	gameField = creatGameField(); 
 	activePlayer = 0;
 	renderBoard(gameField);
 };
+
+function isWinningSituation(){
+	const N = gameField.length;
+	
+	for (let i=0; i<N; ++i){
+		if (isWinningSeguence(i, i+1,0,0,N,1) ||
+		isWinningSeguence(0,N,1,i,i+1,0)
+		){
+			return true;
+		}
+	}
+	if (isWinningSeguence(0,N,1,0,N,1) ||
+		isWinningSeguence(N-1, -1, -1, 0, N, 1)
+		){
+			return true;
+		}
+	
+	return true;
+}
+
 function isWinningSeguence(r0, r1, ri, c0, c1, ci) {
 	let  firstSymbol = null;
-	for (
-	let r = r0, c = c0;
+	for (let r = r0, c = c0;
 	Math.abs(r1-r)> 0 && Math.ads(c1-c)>0;
 	r += ri, c+=c1
 	){
@@ -37,33 +57,15 @@ function isWinningSeguence(r0, r1, ri, c0, c1, ci) {
 	
 	
 }
-function isWinningSituation(){
-	const N = gameField.length;
-	
-	for (let i=0; i<N; ++i){
-		if (isWinningSeguence(i, i+1,0,0,N,1) ||
-		isWinningSeguence(0,N,1,i,i+1,0)
-		){
-			return true;
-		}
-	}
-	if (isWinningSeguence(0,N,1,0,N,1) ||
-		isWinningSeguence(N-1, -1, -1, 0, N, 1)
-		){
-			return true;
-		}
-	
-	return true;
-}
+
 function click(row, column) {
 	const playerSymbol = players [activePlayer];
 	gameField [row][column] = playerSymbol;
 	renderBoard(gameField);
-	
 	if (isWinningSituation()){
 		showWinner(activePlayer);
-	}
+	} else {
 	activePlayer = (activePlayer + 1) % players.length;
-	
+	}
 }
 
